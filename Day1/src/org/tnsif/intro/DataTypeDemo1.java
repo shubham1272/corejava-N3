fa81bac64212bd2cf7dbdb9ec644b94fc9980850
Student <Student@IOE-AIDS-180>
2023-02-02 13:13:51 +0530

    first commit

---
+++ Day1/src/org/tnsif/intro/DataTypeDemo1.java
@@ -0,0 +1,32 @@
+package org.tnsif.intro;
+
+public class DataTypeDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a= 127;
+		int b= 128;
+		float c= 12/5;
+		float d=5/2.5f;
+		double e=12/5;
+		double f=12d/5d;
+		int x='A';
+		int g='z';
+		char y='S';
+		long z=5648648987L;
+		String s="Shubham";
+		System.out.println("byte :"+ a);
+		System.out.println("int :"+b);
+		System.out.println("float :"+c);
+		System.out.println("float :"+ d);
+		System.out.println("double :"+e);
+		System.out.println("double :"+f);
+		System.out.println("ASCII :"+x);
+		System.out.println("Long :"+z);
+		System.out.println("String :"+s);
+		System.out.println("Char :"+y);
+		System.out.println("ASCII :"+g);
+		
+	}
+
+}
---
+++ Day1/src/org/tnsif/intro/UserInputDemo.java
@@ -0,0 +1,23 @@
+package org.tnsif.intro;
+import java.util.*;
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		/*int a=s.nextInt();
+		float b=s.nextInt();
+		char c=s.next().charAt(0);
+		//String d=s.next();*/
+		String e=s.nextLine();
+		//double f=s.nextDouble();
+		//System.out.println("The value of A is:"+a);
+		//System.out.println("The value of b is:"+b);
+		//System.out.println("The value of c is:"+c);
+		//System.out.println("The value of d is:"+d);
+		System.out.println("The value of e is:"+e);
+		//System.out.println("The value of f is:"+f);
+		s.close();
+	}
+
+}
